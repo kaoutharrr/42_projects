25c8d2c450f03d9715e74274963341f44c8521f6
Kaouthar Kouaz <kkouaz@c4r7p3.1337.ma>
2024-01-05 15:43:41 +0100

    ex00 done

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: kkouaz <kkouaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 11:06:01 by kkouaz            #+#    #+#             */
-/*   Updated: 2024/01/04 22:59:10 by kkouaz           ###   ########.fr       */
+/*   Updated: 2024/01/05 15:40:22 by kkouaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include<fstream>
 #include<map>
 #include<string.h>
+#include<cstdlib>
+
 
 void check(std :: string str);
 void fill_map(std :: map <std :: string, float > &data);
